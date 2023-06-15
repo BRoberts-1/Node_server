46beccd4ebf87de94d27998e4c6ac9d2b2ef1e58
Binyamin Roberts <jzroberts@hotmail.com>
2023-06-15 12:40:00 +0300

    get request w/random and id

--- app.js
+++ app.js
@@ -7,4 +7,12 @@ app.get("/sayings", (req, res) => {
   res.json(sayings);
 });
 
+app.get("/sayings/random", (req, res) => {
+  res.json(sayings[Math.floor(Math.random() * sayings.length)]);
+});
+
+app.get("/sayings/:id", (req, res) => {
+  res.json(sayings.find((saying) => saying.id == req.params.id));
+});
+
 module.exports = app;
